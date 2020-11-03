8477be7220bc0b9df49b45b46e624d6c8e8a6d1c
odong-Tree <ss12357@naver.com>
2020-11-03 14:53:04 +0900

    Add new project

---
+++ git test/git test/main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  git test
+//
+//  Created by 김동빈 on 2020/11/03.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+func multiplication(num: Int){
+    print("구구단을 외자")
+}
